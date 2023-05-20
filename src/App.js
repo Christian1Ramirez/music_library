74e14bbee2efcd9587da91c95dbe2fab920358d5
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-05-20 13:13:58 -0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,53 +1,70 @@
-import { useState, useRef } from 'react';
-import Gallery from './components/Gallery';
-import SearchBar from './components/SearchBar'
-import { DataContext } from './contexts/DataContext';
-import { SearchContext } from './contexts/SearchContext';
-import './App.css'
+import { useState, useRef } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Gallery from "./components/Gallery";
+import SearchBar from "./components/SearchBar";
+import { DataContext } from "./contexts/DataContext";
+import { SearchContext } from "./contexts/SearchContext";
+import AlbumView from "./components/AlbumView";
+import ArtistView from "./components/ArtistView";
+import "./App.css";
 
 function App() {
-  let [message, setMessage] = useState('Search for Musix')
+  let [message, setMessage] = useState("Search for Musix");
   let [data, setData] = useState([]);
-  let searchInput = useRef('');
-
+  let searchInput = useRef("");
 
   const handleSearch = (e, search) => {
     e.preventDefault();
     const fetchData = async () => {
       try {
         document.title = `${search} Music`;
-        const response = await fetch(`https://itunes.apple.com/search?term=${search}`)
+        const response = await fetch(
+          `https://itunes.apple.com/search?term=${search}`
+        );
         const resData = await response.json();
         console.log(resData);
-        if(resData.results.length) {
+        if (resData.results.length) {
           setData(resData.results);
-          setMessage('')
+          setMessage("");
         } else {
-          setMessage(`We could find nothing for "${search}"`)
+          setMessage(`We could find nothing for "${search}"`);
         }
-      } catch(e) {
-        console.log(e)
+      } catch (e) {
+        console.log(e);
       }
-    }
+    };
     if (search) {
-      fetchData()
+      fetchData();
     }
-  }
+  };
 
   return (
     <div className="App">
-      <SearchContext.Provider value={
-        {
-          term: searchInput,
-          handleSearch
-        }
-      }>
-      <SearchBar/>
-      </SearchContext.Provider>
-      { message }
-      <DataContext.Provider value={data}>
-        <Gallery/>
-      </DataContext.Provider>
+      {message}
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <SearchContext.Provider
+                  value={{
+                    term: searchInput,
+                    handleSearch,
+                  }}
+                >
+                  <SearchBar />
+                </SearchContext.Provider>
+                <DataContext.Provider value={data}>
+                  <Gallery />
+                </DataContext.Provider>
+              </>
+            }
+          />
+          <Route path="/album/:id" element={<AlbumView />} />
+          <Route path="/artist/:id" element={<ArtistView />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
